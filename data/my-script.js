7414936bdf16e044738b55e0610fb0b4c5196df0
Iryna Volkodav <for.iren86@gmail.com>
2014-06-21 15:23:00 +0300

    To release ...

--- data/my-script.js
+++ data/my-script.js
@@ -1,17 +1,15 @@
-var keywords = ["путин", "путін", "putin", "putyn"];
+//
+// @author: Iryna V.
+//
 
-function contains(value, arr) {
-	return $.inArray(value.toLowerCase(), arr) >= 0;
-}
+var keywords = ["путин", "путін", "putin", "putyn"];
 
 // "self" is a global object in content scripts
 // Listen for a message, and replace the document's
 // contents with the message payload.
 self.port.on("replaceShit", function(message) {
-	console.log("!!! Before handler !!!");
-
 	$.each(keywords, function(index, value) {
-		console.log("*** Replace: ", value);
+		// console.log("*** Replace: ", value);
 		try {
 			var replaced = $("body").html().replace(new XRegExp(value, "ig"), message);
 			$("body").html(replaced);
@@ -19,6 +17,4 @@ self.port.on("replaceShit", function(message) {
 			console.log("!!! Error: ", e);
 		}
 	});
-
-	console.log("!!! After handler !!!");
 });

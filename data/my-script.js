183261ee1024f65ec30656c32ef469f599fbe0a4
Iryna Volkodav <for.iren86@gmail.com>
2014-06-21 15:06:05 +0300

    Pre release

--- data/my-script.js
+++ data/my-script.js
@@ -1,24 +1,34 @@
-var keywords = ["putin", "путин"];
+var keywords = ["путин", "путін", "putin", "putyn"];
+
+function contains(value, arr) {
+	return $.inArray(value.toLowerCase(), arr) >= 0;
+}
 
 // "self" is a global object in content scripts
 // Listen for a message, and replace the document's
 // contents with the message payload.
 self.port.on("replaceShit", function(message) {
 	console.log("!!! Before handler !!!");
-	// $('body').html( $('body').html().replace(new RegExp(keyword, "ig"),'<span style=color:red>  "' + message + '" </span>'));
 
-	$('body').contents().filter(function() {
-	    return this.nodeType == 3;
-	}).each(function(){
-		var str = this.textContent;
-		var res = new RegExp(keyword, "ig").test(str);
-		if (res) {
-			console.log("!!! Matched: ", str);
-		} else {
-			console.log("--- Not Matched: ", str);
+	// $('body').find('*')
+	//   .contents()
+	//   .filter(function(){
+	//     return this.nodeType !== 1;
+	//   })
+	//   .wrap("<span style='color:red;'></span>");
+
+	// $("*").each(function () {
+ //        $(this).text($(this).text().replace(new RegExp(value, "ig"), message));
+	// });
+
+	$.each(keywords, function(index, value) {
+		console.log("*** Replace: ", value);
+		try {
+			var replaced = $("body").html().replace(new XRegExp(value, "ig"), message);
+			$("body").html(replaced);
+		} catch (e) {
+			console.log("!!! Error: ", e);
 		}
-
-	    this.textContent = this.textContent.replace(new RegExp(keyword, "ig"),'<span style=color:red>  "' + message + '" </span>');
 	});
 
 	console.log("!!! After handler !!!");

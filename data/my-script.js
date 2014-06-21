b785dab6129b3db1d5786792f074487e81b818e4
Iryna Volkodav <for.iren86@gmail.com>
2014-06-21 13:39:49 +0300

    Doing some experiments regarding of text processing in html

--- data/my-script.js
+++ data/my-script.js
@@ -1,6 +1,25 @@
+var keywords = ["putin", "путин"];
+
 // "self" is a global object in content scripts
 // Listen for a message, and replace the document's
 // contents with the message payload.
 self.port.on("replaceShit", function(message) {
-	$("body").html("<h1>" + message + "</h1>");
+	console.log("!!! Before handler !!!");
+	// $('body').html( $('body').html().replace(new RegExp(keyword, "ig"),'<span style=color:red>  "' + message + '" </span>'));
+
+	$('body').contents().filter(function() {
+	    return this.nodeType == 3;
+	}).each(function(){
+		var str = this.textContent;
+		var res = new RegExp(keyword, "ig").test(str);
+		if (res) {
+			console.log("!!! Matched: ", str);
+		} else {
+			console.log("--- Not Matched: ", str);
+		}
+
+	    this.textContent = this.textContent.replace(new RegExp(keyword, "ig"),'<span style=color:red>  "' + message + '" </span>');
+	});
+
+	console.log("!!! After handler !!!");
 });

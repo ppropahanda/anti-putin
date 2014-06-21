b087fee693644d460a740f97abe59f93029707bc
Iryna Volkodav <for.iren86@gmail.com>
2014-06-21 18:24:18 +0300

    Removed unused param

--- data/my-script.js
+++ data/my-script.js
@@ -6,15 +6,16 @@ var keywordsDict = {
     "путин": "Хула-ла",
     "пу́тин": "Хула-ла",
     "путін": "Хула-ла",
+    "пу́тін": "Хула-ла",
     "putin": "Hula-la",
     "putyn": "Hula-la"};
 
 // "self" is a global object in content scripts
 // Listen for a message, and replace the document's
 // contents with the message payload.
-self.port.on("replaceShit", function (param) {
+self.port.on("replaceShit", function () {
     $.each(keywordsDict, function (key, value) {
-        // console.log("*** Replace: " + key + " with " + value);
+        console.log("*** Replace: " + key + " with " + value);
         try {
             var replaced = $("body").html().replace(new XRegExp(key, "ig"), value);
             $("body").html(replaced);

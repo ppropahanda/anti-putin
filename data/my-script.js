0a0fb144d9bfacf8274a6ed4acbf4e8cee367395
Iryna Volkodav <for.iren86@gmail.com>
2014-06-21 17:42:09 +0300

    Switched data structure to use dictionary instead of array for replacement

--- data/my-script.js
+++ data/my-script.js
@@ -2,19 +2,24 @@
 // @author: Iryna V.
 //
 
-var keywords = ["путин", "пу́тин", "путін", "putin", "putyn"];
+var keywordsDict = {
+    "путин": "Хула-ла",
+    "пу́тин": "Хула-ла",
+    "путін": "Хула-ла",
+    "putin": "Hula-la",
+    "putyn": "Hula-la"};
 
 // "self" is a global object in content scripts
 // Listen for a message, and replace the document's
 // contents with the message payload.
-self.port.on("replaceShit", function(message) {
-	$.each(keywords, function(index, value) {
-		// console.log("*** Replace: ", value);
-		try {
-			var replaced = $("body").html().replace(new XRegExp(value, "ig"), message);
-			$("body").html(replaced);
-		} catch (e) {
-			console.log("!!! Error: ", e);
-		}
-	});
+self.port.on("replaceShit", function (param) {
+    $.each(keywordsDict, function (key, value) {
+        // console.log("*** Replace: " + key + " with " + value);
+        try {
+            var replaced = $("body").html().replace(new XRegExp(key, "ig"), value);
+            $("body").html(replaced);
+        } catch (e) {
+            console.log("!!! Error: ", e);
+        }
+    });
 });

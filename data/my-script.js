b9753a310a6da13b13fed7570464f0ea596090d6
Iryna Volkodav <for.iren86@gmail.com>
2014-06-22 17:33:20 +0300

    Fixed issue with links. Special thanks to http://dou.ua/users/oksana-kyrychenko/

--- data/my-script.js
+++ data/my-script.js
@@ -2,17 +2,44 @@
 // @author: Iryna V.
 //
 
+function replace(pattern, replacement) {
+    try {
+        var replacedHtml = $("body").html().replace(new XRegExp(pattern, "ig"), replacement);
+        $("body").html(replacedHtml);
+    } catch (e) {
+        console.log("!!! Error: ", e);
+    }
+}
+
+function replaceInWrappedHtmlElement(pattern, replacement, wrapperHtmlElement) {
+    var startElement = "<" + wrapperHtmlElement + ">";
+    var endElement = "</" + wrapperHtmlElement + ">";
+    var patternStartElement = "\\<" + wrapperHtmlElement + "\\>";
+    var patternEndElement = "\\<\\/" + wrapperHtmlElement + "\\>";
+
+    var fixedPattern = patternStartElement + pattern + patternEndElement;
+    var fixedReplacement = startElement + replacement + endElement;
+
+    try {
+        var replacedHtml = $("body").html().replace(new XRegExp(fixedPattern, "ig"), fixedReplacement);
+        $("body").html(replacedHtml);
+    } catch (e) {
+        console.log("!!! Error: ", e);
+    }
+}
+
 // "self" is a global object in content scripts
 // Listen for a message, and replace the document's
 // contents with the message payload.
 self.port.on("replaceShit", function (keywordsDict) {
     $.each(keywordsDict, function (key, value) {
         // console.log("*** Replace: " + key + " with " + value);
-        try {
-            var replaced = $("body").html().replace(new XRegExp(key, "ig"), value);
-            $("body").html(replaced);
-        } catch (e) {
-            console.log("!!! Error: ", e);
-        }
+        replace("\\s+" + key, " " + value);
+        replace(key + "\\s+", value + " ");
+
+        replaceInWrappedHtmlElement(key, value, "strong");
+        replaceInWrappedHtmlElement(key, value, "em");
+        replaceInWrappedHtmlElement(key, value, "b");
+        replaceInWrappedHtmlElement(key, value, "i");
     });
 });

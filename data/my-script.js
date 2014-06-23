6ddad5f88c773cf1b1f53327f093b6c075fe2300
Iryna Volkodav <for.iren86@gmail.com>
2014-06-23 23:50:25 +0300

    Implementing another solution for text processing

--- data/my-script.js
+++ data/my-script.js
@@ -34,6 +34,41 @@ function replaceInWrappedHtmlElement(html, pattern, replacement, wrapperHtmlElem
     return replacedHtml;
 }
 
+function doReplace(keywordsDict) {
+    var html = $("body").html();
+
+    $.each(keywordsDict, function (key, value) {
+        // console.log("*** Replace: " + key + " with " + value);
+
+        try {
+            $("body").find(":not(iframe)").addBack().contents().filter(function () {
+                return (this.nodeType === 1 && this.childNodes) && !/(script|style)/i.test(this.tagName);
+            }).each(function () {
+//                var text = $(this).text();
+//                console.log("text: ", text);
+//                console.log("tagName: ", this.tagName);
+            });
+
+//            var keysArr = Object.keys(keywordsDict);
+//            console.log(keysArr);
+//            $("body").highlight(keysArr);
+        } catch (e) {
+            console.log("!!! Error: ", e);
+        }
+
+//        html = replace(html, "\\s+" + key, " " + value);
+//        html = replace(html, key + "\\s+", value + " ");
+//
+//        html = replaceInWrappedHtmlElement(html, key, value, "span");
+//        html = replaceInWrappedHtmlElement(html, key, value, "strong");
+//        html = replaceInWrappedHtmlElement(html, key, value, "em");
+//        html = replaceInWrappedHtmlElement(html, key, value, "b");
+//        html = replaceInWrappedHtmlElement(html, key, value, "i");
+    });
+
+    $("body").html(html);
+}
+
 // "self" is a global object in content scripts
 // Listen for a message, and replace the document's
 // contents with the message payload.
@@ -41,19 +76,5 @@ self.port.on("loaded", function (keywordsDict) {
     var docUrl = document.location.toString();
     var docTitle = document.title;
 
-    var html = $("body").html();
-
-    $.each(keywordsDict, function (key, value) {
-        // console.log("*** Replace: " + key + " with " + value);
-        html = replace(html, "\\s+" + key, " " + value);
-        html = replace(html, key + "\\s+", value + " ");
-
-        html = replaceInWrappedHtmlElement(html, key, value, "span");
-        html = replaceInWrappedHtmlElement(html, key, value, "strong");
-        html = replaceInWrappedHtmlElement(html, key, value, "em");
-        html = replaceInWrappedHtmlElement(html, key, value, "b");
-        html = replaceInWrappedHtmlElement(html, key, value, "i");
-    });
-
-    $("body").html(html);
+    doReplace(keywordsDict);
 });
